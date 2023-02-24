50fd11ceaad523346d3e4a5e6b6bada6ffa1aebb
Ed Abbondanzio <me@eddieabbondanz.io>
2023-02-24 11:43:04 -0500

    Promisify sqlite db

---
+++ server/src/db.js
@@ -0,0 +1,43 @@
+import sqlite3 from "sqlite3";
+import { promisify } from "node:util";
+
+export const DB_FILE_NAME = "./calls.db";
+
+export async function getDB() {
+  const db = await openDB(DB_FILE_NAME);
+
+  db.run = promisify(db.run);
+  db.get = promisify(db.get);
+  db.all = promisify(db.all);
+  db.each = promisify(db.each);
+  db.exec = promisify(db.exec);
+  db.prepare = promisify(db.prepare);
+  db.serialize = promisify(db.serialize);
+  db.parallelize = promisify(db.parallelize);
+
+  return db;
+}
+
+async function openDB(fileName) {
+  return new Promise((res, rej) => {
+    const db = new sqlite3.Database(fileName, err => {
+      if (err) {
+        rej(err);
+      } else {
+        res(db);
+      }
+    });
+  });
+}
+
+export async function initDB(db) {
+  await db.run(`
+    CREATE TABLE IF NOT EXISTS calls(
+      id INTEGER PRIMARY KEY,
+      call_id TEXT,
+      sdp TEXT,
+      expires_at DATETIME,
+      link_was_opened BOOLEAN
+    );
+  `);
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,102 @@
-import express from "express";
-const app = express();
+import { WebSocketServer } from "ws";
+import { addHours } from "date-fns";
+import { getDB, initDB } from "./db.js";
 
-const PORT = 3000;
+const PORT = 8080;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const MessageType = Object.freeze({
+  RegisterCall: "register-call",
+  GetSDP: "get-sdp",
+  VideoOffer: "video-offer",
 });
 
-console.log("Starting server");
+async function main() {
+  const db = await getDB();
+  await initDB(db);
+  console.log(await db.get("select * from calls"));
 
-app.listen(PORT, () => {
+  const wss = new WebSocketServer({ port: PORT });
   console.log(`Listening on ${PORT}`);
-});
+
+  wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+
+    ws.on("message", async function message(data) {
+      const json = JSON.parse(data);
+
+      switch (json.type) {
+        // Calls are registered in the database because the callee won't be listening
+        // immediately as they don't have the link uet.
+        case MessageType.RegisterCall:
+          console.log("REGISTER CALL: ", json);
+          const { callID, sdp } = json;
+          const expiresAt = addHours(new Date(), 1);
+
+          await insertCall(db, {
+            callID,
+            sdp,
+            expiresAt,
+            wasConnected: false,
+          });
+          break;
+
+        case MessageType.VideoOffer:
+          console.log("Video offer");
+          break;
+
+        case MessageType.GetSDP:
+          const requestedCallID = json.callID;
+          const foundSDP = await getSDP(db, requestedCallID);
+          console.log({ foundSDP });
+
+          if (foundSDP) {
+            ws.send(
+              JSON.stringify({
+                type: MessageType.GetSDP,
+                sdp: foundSDP.sdp,
+              }),
+            );
+          }
+          break;
+      }
+    });
+  });
+}
+main();
+
+// TODO: Wrap db in promises instead of this...
+
+async function getSDP(db, callID) {
+  return new Promise((res, rej) => {
+    db.get(`SELECT * from calls where call_id = ?`, [callID], (err, row) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(row);
+      }
+    });
+  });
+}
+
+async function insertCall(db, call) {
+  return new Promise((res, rej) => {
+    const { callID, sdp, expiresAt, wasConnected } = call;
+
+    db.run(
+      `
+      INSERT INTO calls 
+        (call_id, sdp, expires_at, link_was_opened) 
+        values 
+        (?, ?, ?, ?);
+    `,
+      [callID, sdp, expiresAt, wasConnected],
+      err => {
+        if (err) {
+          rej(err);
+        } else {
+          res();
+        }
+      },
+    );
+  });
+}
