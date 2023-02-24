94d0d672afc7c6c147e821dfcbdf2da2b9d2801b
Ed Abbondanzio <me@eddieabbondanz.io>
2023-02-24 12:59:54 -0500

    Use React for front end

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div className="fc fg1">
+      <h1 className="asc">VideoUp</h1>
+
+      {/* Content */}
+      <div className="fc fg1"></div>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,109 +1,116 @@
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 
-const CALL_ID_LENGTH = 8;
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-// https://gist.github.com/zziuni/3741933
-const STUN_SERVERS = [
-  "stun.l.google.com:19302",
-  "stun1.l.google.com:19302",
-  "stun2.l.google.com:19302",
-  "stun3.l.google.com:19302",
-  "stun4.l.google.com:19302",
-];
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
 
-async function main() {
-  const ws = await openWSConnection("ws://localhost:8080");
-  ws.addEventListener("message", ({ data }) => {
-    console.log(data);
-    const json = JSON.parse(data);
+// const CALL_ID_LENGTH = 8;
 
-    switch (json.type) {
-      case "get-sdp":
-        console.log("GOT SDP: ", json.sdp);
-        break;
-    }
-  });
+// // https://gist.github.com/zziuni/3741933
+// const STUN_SERVERS = [
+//   "stun.l.google.com:19302",
+//   "stun1.l.google.com:19302",
+//   "stun2.l.google.com:19302",
+//   "stun3.l.google.com:19302",
+//   "stun4.l.google.com:19302",
+// ];
 
-  // If a query param was set, it means we're trying to join an existing call, otherwise
-  // we want to host a new one.
-  let callID = new URL(window.location.href).searchParams.get("callID");
-  let isHost = callID == null;
-  if (isHost) {
-    callID = nanoid(CALL_ID_LENGTH);
-  } else {
-    console.log("REQUEST SDP FOR CALL ID: ", callID);
-    ws.send(
-      JSON.stringify({
-        type: "get-sdp",
-        callID,
-      }),
-    );
-  }
+// async function main() {
+//   const ws = await openWSConnection("ws://localhost:8080");
+//   ws.addEventListener("message", ({ data }) => {
+//     console.log(data);
+//     const json = JSON.parse(data);
 
-  const [video, audio] = await getVideoAndAudioTracks();
-  const pc = await createRtcPeerConnection(ws, callID, isHost, video, audio);
-}
-main();
+//     switch (json.type) {
+//       case "get-sdp":
+//         console.log("GOT SDP: ", json.sdp);
+//         break;
+//     }
+//   });
 
-async function openWSConnection(url) {
-  const ws = new WebSocket(url);
+//   // If a query param was set, it means we're trying to join an existing call, otherwise
+//   // we want to host a new one.
+//   let callID = new URL(window.location.href).searchParams.get("callID");
+//   let isHost = callID == null;
+//   if (isHost) {
+//     callID = nanoid(CALL_ID_LENGTH);
+//   } else {
+//     console.log("REQUEST SDP FOR CALL ID: ", callID);
+//     ws.send(
+//       JSON.stringify({
+//         type: "get-sdp",
+//         callID,
+//       }),
+//     );
+//   }
 
-  return new Promise(res => {
-    ws.addEventListener("open", () => res(ws));
-  });
-}
+//   const [video, audio] = await getVideoAndAudioTracks();
+//   const pc = await createRtcPeerConnection(ws, callID, isHost, video, audio);
+// }
+// main();
 
-async function createRtcPeerConnection(ws, callID, isHost, video, audio) {
-  const pc = new RTCPeerConnection({});
-  pc.addTrack(video);
-  pc.addTrack(audio);
+// async function openWSConnection(url) {
+//   const ws = new WebSocket(url);
 
-  // Listen for when we find an ICE candidate so we can notify the other peer.
-  pc.addEventListener("icecandidate", ev => {
-    console.log("ICE CANDIDATE: ", ev);
-  });
+//   return new Promise(res => {
+//     ws.addEventListener("open", () => res(ws));
+//   });
+// }
 
-  // Listen for incoming tracks
-  pc.addEventListener("track", ev => {
-    console.log("TRACK: ", ev);
-  });
+// async function createRtcPeerConnection(ws, callID, isHost, video, audio) {
+//   const pc = new RTCPeerConnection({});
+//   pc.addTrack(video);
+//   pc.addTrack(audio);
 
-  // Listen for when we need to restart the connection process.
-  // (negotiationneeded is triggered when we add tracks to the stream)
-  pc.addEventListener("negotiationneeded", async () => {
-    console.log("Negotiation needed!");
-    const offer = pc.createOffer();
-    await pc.setLocalDescription(offer);
+//   // Listen for when we find an ICE candidate so we can notify the other peer.
+//   pc.addEventListener("icecandidate", ev => {
+//     console.log("ICE CANDIDATE: ", ev);
+//   });
 
-    if (isHost) {
-      // Notify signaling server so we can pass the SDP offer to the other peer.
-      ws.send(
-        JSON.stringify({
-          type: "register-call",
-          callID,
-          sdp: pc.localDescription,
-        }),
-      );
-    }
-  });
+//   // Listen for incoming tracks
+//   pc.addEventListener("track", ev => {
+//     console.log("TRACK: ", ev);
+//   });
 
-  // Optionals
-  // TODO: Decide if needed.
-  pc.addEventListener("iceconnectionstatechange", ev => {
-    console.log("ICE STATE CHANGE: ", ev);
-  });
+//   // Listen for when we need to restart the connection process.
+//   // (negotiationneeded is triggered when we add tracks to the stream)
+//   pc.addEventListener("negotiationneeded", async () => {
+//     console.log("Negotiation needed!");
+//     const offer = pc.createOffer();
+//     await pc.setLocalDescription(offer);
 
-  return pc;
-}
+//     if (isHost) {
+//       // Notify signaling server so we can pass the SDP offer to the other peer.
+//       ws.send(
+//         JSON.stringify({
+//           type: "register-call",
+//           callID,
+//           sdp: pc.localDescription,
+//         }),
+//       );
+//     }
+//   });
 
-async function getVideoAndAudioTracks() {
-  const mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
+//   // Optionals
+//   // TODO: Decide if needed.
+//   pc.addEventListener("iceconnectionstatechange", ev => {
+//     console.log("ICE STATE CHANGE: ", ev);
+//   });
 
-  const [videoTrack] = mediaStream.getVideoTracks();
-  const [audioTrack] = mediaStream.getAudioTracks();
+//   return pc;
+// }
 
-  return [videoTrack, audioTrack];
-}
+// async function getVideoAndAudioTracks() {
+//   const mediaStream = await navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: true,
+//   });
+
+//   const [videoTrack] = mediaStream.getVideoTracks();
+//   const [audioTrack] = mediaStream.getAudioTracks();
+
+//   return [videoTrack, audioTrack];
+// }
