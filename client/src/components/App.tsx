3c8b22a48ceac49bfd2d9f77ecf53a742541493a
Ed Abbondanzio <me@eddieabbondanz.io>
2023-03-04 21:37:51 -0500

    Make html content full size of screen

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,59 +1,47 @@
-// @ts-nocheck
-
 import { nanoid } from "nanoid";
 import { useState } from "react";
 import { VideoChat } from "./VideoChat";
 import { NavBar } from "./NavBar";
+import styled from "styled-components";
 
 const CALL_ID_LENGTH = 8;
 const CALL_ID_SEARCH_PARAM = "callID";
 
-export function App(props) {
-  const { ws } = props;
-  let url = new URL(window.location.href);
+export function App() {
+  // const { ws } = props;
+  // let url = new URL(window.location.href);
 
-  let [callID, setCallID] = useState(
-    url.searchParams.get(CALL_ID_SEARCH_PARAM),
-  );
-  let [isHost] = useState(callID == null);
-  let [inviteURL, setInviteURL] = useState(null);
-  console.log("Is host: ", isHost);
+  // let [callID, setCallID] = useState(
+  //   url.searchParams.get(CALL_ID_SEARCH_PARAM),
+  // );
+  // let [isHost] = useState(callID == null);
+  // let [inviteURL, setInviteURL] = useState(null);
+  // console.log("Is host: ", isHost);
 
-  if (isHost && callID == null) {
-    const cID = nanoid(CALL_ID_LENGTH);
+  // if (isHost && callID == null) {
+  //   const cID = nanoid(CALL_ID_LENGTH);
 
-    setCallID(cID);
-    setInviteURL(`${url.href}?${CALL_ID_SEARCH_PARAM}=${cID}`);
-  }
+  //   setCallID(cID);
+  //   setInviteURL(`${url.href}?${CALL_ID_SEARCH_PARAM}=${cID}`);
+  // }
 
   // N.B. Because of browser auto play policies we have to get the guest to
   // interact with the page before we can play video so we add a join button to
   // get them to click.
-  let [userClickedJoin, setUserClickedJoin] = useState(isHost);
+  // let [userClickedJoin, setUserClickedJoin] = useState(isHost);
 
   return (
-    <div className="fc fg1">
+    <>
       <NavBar />
-
-      <span class="tag is-black">Black</span>
-      <span class="tag is-dark">Dark</span>
-      <span class="tag is-light">Light</span>
-      <span class="tag is-white">White</span>
-      <span class="tag is-primary">Primary</span>
-      <span class="tag is-link">Link</span>
-      <span class="tag is-info">Info</span>
-      <span class="tag is-success">Success</span>
-      <span class="tag is-warning">Warning</span>
-      <span class="tag is-danger">Danger</span>
-
-      <div className="fc fg1">{inviteURL}</div>
-      {/* {userClickedJoin && <VideoChat ws={ws} callID={callID} isHost={isHost} />} */}
-      {/* {!userClickedJoin && (
-        <div>
-          Click to start the video chat:
-          <button onClick={() => setUserClickedJoin(true)}>Join</button>
-        </div>
-      )} */}
-    </div>
+    </>
   );
 }
+
+// {/* <div className="fc fg1">{inviteURL}</div> */}
+// {/* {userClickedJoin && <VideoChat ws={ws} callID={callID} isHost={isHost} />} */}
+// {/* {!userClickedJoin && (
+//   <div>
+//     Click to start the video chat:
+//     <button onClick={() => setUserClickedJoin(true)}>Join</button>
+//   </div>
+// )} */}
